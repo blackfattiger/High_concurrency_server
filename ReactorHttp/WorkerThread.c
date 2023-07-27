89cbd8a27b20cb6beb350e73b05de988578eca5b
blackfattiger <79746439+blackfattiger@users.noreply.github.com>
2023-07-27 22:59:05 +0800

    Add files via upload

--- ReactorHttp/WorkerThread.c
+++ ReactorHttp/WorkerThread.c
@@ -20,7 +20,7 @@ void* subThreadRunning(void* arg)
     pthread_mutex_lock(&thread->mutex);
     thread->evLoop = eventLoopInitEx(thread->name);
     pthread_mutex_unlock(&thread->mutex);
-    pthread_cond_signal(&thread->cond);
+    pthread_cond_signal(&thread->cond); //通知主线程解除阻塞
     eventLoopRun(thread->evLoop);
     return NULL;
 }
@@ -29,5 +29,13 @@ void wokerThreadRun(struct WorkerThread* thread)
 {
     //创建子线程
     pthread_create(&thread->threadID, NULL, subThreadRunning, thread);
-
+    //由于不知道子线程是否将EventLoop成功实例化，所欲需要阻塞主线程
+    //阻塞主线程，让当前函数不会直接结束
+    pthread_mutex_lock(&thread->mutex);
+    while (thread->evLoop == NULL) // 主线程访问的是子线程的evLoop，属于访问共享资源，所以需要同步
+    {//创建EventLoop可能需要一定时间，所以需要while循环一直监控，如果使用if判断，可能还是为NULL
+       //此处除了条件变量，也可以使用信号量，这二者都可以起到阻塞线程的作用
+        pthread_cond_wait(&thread->cond, &thread->mutex);//被条件变量阻塞
+    }
+    pthread_mutex_unlock(&thread->mutex);
 }
